a903289c6c59823e5014c418252b01e3e6a74203
bioburn <jackey@umail.ucsb.edu>
2016-02-21 07:26:33 +0900

    JL added text field and button to take a seed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,11 @@ class WelcomeController < ApplicationController
   def index
 
 	@qd = '{"version" : "0.1","questions": [{"question": "binHexOctDec","repeat": 5}]}'
-    @seed = 'abcd1234'
-    request = '{"descriptor:"' + @qd + ', "seed":' + @seed + '}'
-    @url = 'https://pa-service-prod.herokuapp.com/v1/build_quiz'
-    @responseJson = HTTP.post(@url, :json => {:descriptor => ActiveSupport::JSON.decode(@qd), :seed => @seed}).to_s 
- 
+    @seed = params[:seed] ? params[:seed] : 'abcd1234'
+
+    @url = 'https://pa-service-prod.herokuapp.com/v1/generate'
+    @responseJson = HTTP.post(@url, :json => {:type => 'json' ,:qd => ActiveSupport::JSON.decode(@qd), :seed => @seed}).to_s
+
   end
 
 end
