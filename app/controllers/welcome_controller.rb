280f46521e0278f93cd2e4c322d81e895234e6dd
bioburn <jackey@umail.ucsb.edu>
2016-02-21 10:05:26 +0900

    JL task 3, check and display seed, generate random seed if none provided

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,19 @@
 require "http"
+require 'securerandom'
 
 class WelcomeController < ApplicationController
 
   def index
 
-	@qd = '{"version" : "0.1","questions": [{"question": "binHexOctDec","repeat": 5}]}'
-    @seed = params[:seed] ? params[:seed] : 'abcd1234'
+	  @qd = '{"version" : "0.1","questions": [{"question": "mc-change-of-base","repeat": 5}]}'
+    @random = SecureRandom.hex(4)
+    @seed = params[:seed] ? params[:seed] : @random
 
     @url = 'https://pa-service-prod.herokuapp.com/v1/generate'
     @responseJson = HTTP.post(@url, :json => {:type => 'json' ,:qd => ActiveSupport::JSON.decode(@qd), :seed => @seed}).to_s
 
+    @check = 'https://pa-service-prod.herokuapp.com/v1/check?type=seed&value=' + @seed
+    @checkSeed = HTTP.get(@check).to_s #, :json => {:value => @seed}).to_s
   end
 
 end
